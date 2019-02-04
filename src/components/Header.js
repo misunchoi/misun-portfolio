7c22e04ecffa7a49a1ea0e1769882c22c88fc497
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-04 13:50:55 -0800

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,23 @@ class Header extends Component {
   render() {
     return (
       <div className="Header" style={{paddingBottom: '10px'}}>
-        <div style={{ height: '100px', backgroundColor: 'rgb(117, 195, 173)', color: 'white', fontSize: '70px', margin: 'auto', justifyContent: 'center' }}> Portfolio </div>
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="/home">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="/resume">Resume</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/projects">Projects</a>
-          </li>
-        </ul>
+        <div style={{ height: '100px', backgroundColor: 'black', color: 'white', fontSize: '50px', margin: 'auto', justifyContent: 'center' }}> 
+          Mi Sun Choi <br/>
+          <a style={{ fontSize: '10px', color: 'white' }} href="www.linkedin.com/in/mi-sun-choi">LinkedIn</a>
+        </div>
+        <div>
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="/resume">Resume</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/projects">Projects</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
