162170d51bd7701ac839564b3bdfc9fcb884f9b4
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-04 13:53:38 -0800

    added jumbotron

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,15 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <div class="jumbotron">
+          <h1 class="display-3">Hello, world!</h1>
+          <p class="lead">I'm a web developer seeking for front-end, back-end, full-stack web development positions.</p>
+          <hr class="my-4"/>
+          <p>Explore the projects that I've been part of.</p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="/projects" role="button">Learn more</a>
+          </p>
+        </div>
       </div>
     );
   }
