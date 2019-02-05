fb1e43fac3bb071892b6d4f79f97037223cd6134
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-04 16:34:36 -0800

    added tictactoe and footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header'
+import Footer from './components/Footer'
+
 import Home from './pages/Home'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
+import TicTacToe from './pages/TicTacToe';
 
 
 class App extends Component {
@@ -17,8 +20,10 @@ class App extends Component {
             <Route path="/" exact component={Home} />
             <Route path="/resume" exact component={Resume} />
             <Route path="/projects" exact component={Projects} />
+            <Route path="/tictactoe" exact component={TicTacToe} />
           </div>
         </Router>
+        <Footer />
       </div>
     );
   }
