15581aaa6b27993e7320f3ea97c349594123ed22
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-05 10:59:08 -0800

    edited jumbotron text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,9 @@ class Home extends Component {
           <h1 class="display-3">Hello, world!</h1>
           <p class="lead">I'm a front-end web developer seeking to create interactive and user-friendly websites!</p>
           <hr class="my-4"/>
-          <p>Explore the projects that I've been part of.</p>
+          <p>Explore the various projects I've been working on.</p>
           <p class="lead">
-            <a class="btn btn-primary btn-lg" href="/projects" role="button">Learn more</a>
+            <a class="btn btn-primary btn-lg" href="/projects" role="button">Go to projects</a>
           </p>
         </div>
       </div>
