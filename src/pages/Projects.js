e01b21cb1ae750e1bf5f4134a3437f2412806ddc
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-07 11:01:29 -0800

    edited project description

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,7 +10,7 @@ class Project extends Component {
           <div class="card-header">Gatsby, Moqui</div>
           <div class="card-body">
             <h4 class="card-title">Donation Website</h4>
-            <p class="card-text">Developed a donation page utilizing material-ui components. Visit the website <a style={{ color: 'white' }} href="http://mkdonation.com.s3-website-us-east-1.amazonaws.com/">here.</a></p>
+            <p class="card-text">Used Material-UI, Yup, and Formik for form validation. Moqui serves as the backend for the website. Visit the website <a style={{ color: 'white' }} href="http://mkdonation.com.s3-website-us-east-1.amazonaws.com/">here.</a></p>
             <a href="https://github.com/misunchoi/my-website-gatsby"><button type="button" class="btn btn-secondary">Github</button></a>
           </div>
         </div>
@@ -19,7 +19,7 @@ class Project extends Component {
           <div class="card-header">React.js</div>
           <div class="card-body">
             <h4 class="card-title">TicTacToe</h4>
-            <p class="card-text">Classic TicTacToe game where users can choose an emoji to play. Click <a style={{ color: 'white' }} href="/tictactoe">here</a> to play the game.</p>
+            <p class="card-text">Classic TicTacToe game where users can choose an emoji to play. Click <a style={{ color: 'white' }} href="/tictactoe">here</a> to play the game.</p><br/>
             <a href="https://github.com/misunchoi/tictactoe"><button type="button" class="btn btn-secondary">Github</button></a>
           </div>
         </div>
@@ -28,7 +28,7 @@ class Project extends Component {
           <div class="card-header">Ruby on Rails, React.js</div>
           <div class="card-body">
             <h4 class="card-title">Apartment App</h4>
-            <p class="card-text">Logged in users can create, edit, and delete apartment information.</p><br/>
+            <p class="card-text">Logged in users can create, edit, and delete apartment information.</p><br/><br/>
             <a href="https://github.com/misunchoi?utf8=%E2%9C%93&tab=repositories&q=apartment&type=&language="><button type="button" class="btn btn-secondary">Github</button></a>
           </div>
         </div>
@@ -37,7 +37,7 @@ class Project extends Component {
           <div class="card-header"> React.js</div>
           <div class="card-body">
             <h4 class="card-title">Portfolio Website</h4>
-            <p class="card-text">Built portfolio website using React.js and Bootstrap.</p><br/>
+            <p class="card-text">Developed a portfolio website using React.js and Bootstrap.</p><br/><br/>
             <a href="https://github.com/misunchoi/misun-portfolio"><button type="button" class="btn btn-secondary">Github</button></a>
           </div>
         </div>
