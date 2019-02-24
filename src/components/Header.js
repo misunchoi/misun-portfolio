f165d0ce905c989eb34b2e2184c4b71ed5c76d47
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-23 22:43:00 -0800

    deleted home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,9 +31,6 @@ class Header extends Component {
           <div class={this.state.toggle} id="navbarColor01">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
-              </li>
-              <li class="nav-item">
                 <a class="nav-link" href="/resume">Resume</a>
               </li>
               <li class="nav-item">
