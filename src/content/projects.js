07ff5595e5124512e7f0ca47a32b95c858b822d9
Mi Sun Choi <choi.msc033@gmail.com>
2019-03-07 12:05:55 -0800

    updated projects info

--- src/content/projects.js
+++ src/content/projects.js
@@ -9,7 +9,7 @@ const projects = [
   {
     header: "Gatsby, Moqui",
     title: "GND Website",
-    text: "Created the layout, header, footer, and Start Serving page. Implemented AWS Simple Email Service (SES). ",
+    text: "Created the layout, New Here and Start Serving page. Implemented AWS Simple Email Service (SES). ",
     link: "http://gnd-website.s3-website-us-east-1.amazonaws.com/startserving/",
     github: "https://github.com/tfe-gnd-project/GND-website",
   },
